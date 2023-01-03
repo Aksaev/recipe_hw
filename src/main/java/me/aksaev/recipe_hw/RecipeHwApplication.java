de0b1c1f9aaed34fd5fbc4b5b9e32ce154c7a707
Aksaev <mail-ax@ya.ru>
2023-01-03 18:12:27 +0400

    Initial commit

---
+++ src/main/java/me/aksaev/recipe_hw/RecipeHwApplication.java
@@ -0,0 +1,13 @@
+package me.aksaev.recipe_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/aksaev/recipe_hw/RecipeHwApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.recipe_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
